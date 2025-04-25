64f8bc68f2c1210b918b7a30d7213c417c7dddaf
KingPanda-Development <72608447+KingPanda-Development@users.noreply.github.com>
2025-04-25 13:03:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept',
     );
-    console.log([${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode});
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,7 +35,7 @@ app.all('/player/login/dashboard', function (req, res) {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
         for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
         if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
-    } catch (why) { console.log(Warning: ${why}); }
+    } catch (why) { console.log(`Warning: ${why}`); }
 
     res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
@@ -46,11 +46,11 @@ app.all('/player/growid/login/validate', (req, res) => {
     const password = req.body.password;
 
     const token = Buffer.from(
-        _token=${_token}&growId=${growId}&password=${password},
+        `_token=${_token}&growId=${growId}&password=${password}`,
     ).toString('base64');
 
     res.send(
-        {"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"},
+        `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
 });
 
@@ -59,7 +59,7 @@ app.all('/player/*', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello World! Apa lu');
+    res.send('Hello World!');
 });
 
 app.listen(5000, function () {
