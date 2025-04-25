318bf446af972cccf832a96f0350d775035e68ae
KingPanda-Development <72608447+KingPanda-Development@users.noreply.github.com>
2025-04-25 13:01:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const rateLimiter = require('express-rate-limit');
 const compression = require('compression');
 
-const serverless = require('serverless-http'); // penting buat vercel
-
 app.use(compression({
     level: 5,
     threshold: 0,
@@ -16,9 +14,17 @@ app.use(compression({
         return compression.filter(req, res);
     }
 }));
-
 app.set('view engine', 'ejs');
 app.set('trust proxy', 1);
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    console.log([${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode});
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
@@ -26,21 +32,12 @@ app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n');
-        const uName = uData[0].split('|');
-        const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) {
-            const d = uData[i].split('|');
-            tData[d[0]] = d[1];
-        }
-        if (uName[1] && uPass[1]) {
-            res.redirect('/player/growid/login/validate');
-        }
-    } catch (why) {
-        console.log(`Warning: ${why}`);
-    }
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(Warning: ${why}); }
 
-    res.send(tData); // karena render ejs ga jalan di vercel
+    res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
@@ -48,15 +45,13 @@ app.all('/player/growid/login/validate', (req, res) => {
     const growId = req.body.growId;
     const password = req.body.password;
 
-    const token = Buffer.from(`_token=${_token}&growId=${growId}&password=${password}`).toString('base64');
+    const token = Buffer.from(
+        _token=${_token}&growId=${growId}&password=${password},
+    ).toString('base64');
 
-    res.send({
-        status: "success",
-        message: "Account Validated.",
-        token: token,
-        url: "",
-        accountType: "growtopia"
-    });
+    res.send(
+        {"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"},
+    );
 });
 
 app.all('/player/*', function (req, res) {
@@ -67,5 +62,6 @@ app.get('/', function (req, res) {
     res.send('Hello World! Apa lu');
 });
 
-module.exports = app;
-module.exports.handler = serverless(app); // wajib buat vercel
+app.listen(5000, function () {
+    console.log('Listening on port 5000');
+});
