264b51f86c96094952b4f1b8848aec021635b999
KingPanda-Development <72608447+KingPanda-Development@users.noreply.github.com>
2025-04-30 04:16:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,18 @@ app.all('/player/login/dashboard', function (req, res) {
         if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
     } catch (why) { console.log(`Warning: ${why}`); }
 
-    res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
+    res.render(__dirname + '/public/dashboardgt/dashboard.ejs', { data: tData });
+});
+
+app.post('/player/login/login-legacy', (req, res) => {
+    const tData = {};
+    try {
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
+
+    res.render(__dirname + '/public/dashboardgt/login-legacy.ejs', { data: tData });
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
