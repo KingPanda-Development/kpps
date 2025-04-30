83c18957e1d0c7061640a6d4a0d25dd6e7714c26
KingPanda-Development <72608447+KingPanda-Development@users.noreply.github.com>
2025-04-30 11:24:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ app.all('/player/growid/login/validate', (req, res) => {
     const _token = req.body._token;
     const growId = req.body.growId;
     const password = req.body.password;
+    const saveLogin = req.body.saveLogin;
 
     const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
+        `_token=${_token}&saveLogin=${saveLogin}&growId=${growId}&password=${password}`,
     ).toString('base64');
 
     res.send(
