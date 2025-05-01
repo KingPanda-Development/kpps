09dc8906c8b8062ea3ec1849862434751abd428b
KingPanda-Development <72608447+KingPanda-Development@users.noreply.github.com>
2025-05-01 15:54:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,60 +29,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
-app.all('/player/login/dashboard', function (req, res) {
-    const tData = {};
-    try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('https://kingpandatopia.my.id/player/growid/login/validate'); }
-    } catch (why) { console.log(`Warning: ${why}`); }
-
-    res.render(__dirname + '/public/html/dashboardv2.ejs', { data: tData });
-});
-
-app.all('/player/login/login-legacy', (req, res) => {
-    const tData = {};
-    try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
-    } catch (why) { console.log(`Warning: ${why}`); }
-    console.log(req.body);
-    res.render(__dirname + '/public/dashboardgt/login-legacy.ejs', { data: tData });
-});
-
-app.all('/player/growid/login/validate', (req, res) => {
-    const _token = req.body._token;
-    const growId = req.body.growId;
-    const password = req.body.password;
-    const saveLogin = req.body.saveLogin;
-
-    const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
-    ).toString('base64');
-
-    res.send(
-        `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
-    );
-});
-
-app.all('/hosts', (req, res) => {
-    res.type('text/plain');
-    res.send(
-`178.128.115.218 www.growtopia1.com
-178.128.115.218 www.growtopia2.com
-178.128.115.218 osm.growplus.asia`
-    );
-});
-
-app.all('/player/*', function (req, res) {
-    res.status(301).redirect('https://api.yoruakio.tech/player/' + req.path.slice(8));
-});
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/main/index.html');
+});
+
+app.use((req, res) => {
+    res.status(404).render('/public/main/404.html', {
+        url: req.originalUrl,
+        time: currentTime
+    });
 });
 
 app.listen(5000, function () {
